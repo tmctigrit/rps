abd3744977361599f673e83f44cecde999031b17
Jessica Meyer <JessLynnMeyer@gmail.com>
2014-12-17 19:15:41 -0600

    update endpoints

--- server.rb
+++ server.rb
@@ -13,61 +13,74 @@ end
 
 helpers do 
     def db
-        RPS.create_db_connection('rps')
+      db = RPS.create_db_connection('rps')
     end
 end
 
 # homepage
+
 get '/' do
   erb :index
 end
 
+# user signup
 
-get '/signup' do 
-
+get '/signup' do
+  erb :index
 end
 
-# user signup
 post '/signup' do
-  
+    @user_data = RPS::UsersRepo.save(db, params)
+    session['user_id'] = @user_data['id']
+
+    redirect to '/welcome'
 end
 
 # user signin
-post '/signin' do
-    params = JSON.parse request.body.read
-    
-    username = params['username']
-    password = params['password']
 
-    user = RPS::UsersRepo.find_by_username(username)
+get '/signin' do
+  erb :index
+end
+
+post '/signin' do
+    # params = JSON.parse request.body.read
+    
+    # username = params['username']
+    # password = params['password']
+    
+    @user_login = RPS::UsersRepo.find_by_username(db, params[:username])
+    session['user_id'] = @user_login['id']
+
+    redirect to '/welcome'
 end
 
 # welcome page; user after signin
 get '/welcome' do
+ erb :welcome
+end
 
+post '/welcome' do
+ # play new game 
 end
 
 # new game page
 get '/gamenew' do
- 
+    # choose opponent
  erb :new_game
 end
 
+
 # game over page
 get '/gameover' do
-
-end
-
-# existing games
-get '/gamesexisting' do
-
+  erb :game_over
 end
 
 # game summary
 get '/summary' do
-  
+  erb :summary
 end
 
+# user sign out
 post '/signout' do 
     session.clear
     redirect to '/'
