be6d0ff55952db8665155a0814a2c289d710882f
Jessica Meyer <JessLynnMeyer@gmail.com>
2014-12-17 18:37:47 -0600

    add some stuff to server, update users repo

--- server.rb
+++ server.rb
@@ -2,25 +2,44 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'pg'
 require 'pry-byebug'
-
+require 'rack-flash'
 
 require_relative 'lib/rps.rb'
 
-set :bind, '0.0.0.0'
+configure do
+  enable :sessions 
+  set :bind, '0.0.0.0'
+end 
+
+helpers do 
+    def db
+        RPS.create_db_connection('rps')
+    end
+end
 
 # homepage
 get '/' do
-  
+  erb :index
+end
+
+
+get '/signup' do 
+
 end
 
 # user signup
 post '/signup' do
-
+  
 end
 
 # user signin
 post '/signin' do
+    params = JSON.parse request.body.read
+    
+    username = params['username']
+    password = params['password']
 
+    user = RPS::UsersRepo.find_by_username(username)
 end
 
 # welcome page; user after signin
@@ -30,7 +49,8 @@ end
 
 # new game page
 get '/gamenew' do
-
+ 
+ erb :new_game
 end
 
 # game over page
@@ -45,9 +65,13 @@ end
 
 # game summary
 get '/summary' do
-
+  
 end
 
+post '/signout' do 
+    session.clear
+    redirect to '/'
+end
 
 
 
